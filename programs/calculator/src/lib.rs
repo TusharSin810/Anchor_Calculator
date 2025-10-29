db301744cfba2da715f2420018be5c5fc4b27239
TusharSin810 <tsinghal666@gmail.com>
2025-10-30 00:56:31 +0530

    Intialized Functions

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -5,12 +5,27 @@ declare_id!("5igcP1nawEo1A31YtZSj9z4nqrnbAvhmcQsoHXccwEDD");
 #[program]
 pub mod calculator {
     use super::*;
+    
+    pub fn init(){
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
+    }
+
+    pub fn double(){
+
+    }
+
+    pub fn add(num: u32){
+        
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
+#[account]
+struct DataShape {
+    pub num: u32
+}
+
+pub struct Initialize<'info>{
+    pub account: Account<'info, DataShape>,
+    pub system_program: Program<'info, System>,
+    signer: Signer<'info>
+}
