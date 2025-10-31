0670aa5d31dd020192f7af182205c841e4a4175a
TusharSin810 <tsinghal666@gmail.com>
2025-10-31 23:32:09 +0530

    Finished Contract

--- programs/calculator/src/lib.rs
+++ programs/calculator/src/lib.rs
@@ -2,55 +2,91 @@ use anchor_lang::prelude::*;
 
 declare_id!("5igcP1nawEo1A31YtZSj9z4nqrnbAvhmcQsoHXccwEDD");
 
-#[program]
-pub mod calculator {
-    use super::*;
-    
-    pub fn init(ctx: Context<Initialize>, init_value: u32) -> Result<()> {
-        ctx.accounts.account.num = init_value;
+#[derive(Debug)]
+struct Rect {
+    width: u32,
+    height: u32,
+}
 
+impl Rect {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
+#[program]
+pub mod anchor_calculator {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        ctx.accounts.new_account.data = 1;
+        let rect = Rect {
+            width: 10,
+            height: 20,
+        };
+        println!("Rect: {:?}", rect);
         Ok(())
     }
 
     pub fn double(ctx: Context<Double>) -> Result<()> {
-        ctx.accounts.account.num = ctx.accounts.account.num * 2;
+        ctx.accounts.account.data = ctx.accounts.account.data * 2;
         Ok(())
     }
 
-    pub fn add(ctx: Context<Add>, num: u32) -> Result<()> {
-        ctx.accounts.account.num = ctx.accounts.account.num + num;        
-        Ok(())        
+    pub fn halve(ctx: Context<Halve>) -> Result<()> {
+        ctx.accounts.account.data = ctx.accounts.account.data / 2;
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Add>, amount: u32) -> Result<()> {
+        ctx.accounts.account.data = ctx.accounts.account.data + amount;
+        Ok(())
+    }
+
+    pub fn sub(ctx: Context<Sub>, amount: u32) -> Result<()> {
+        ctx.accounts.account.data = ctx.accounts.account.data - amount;
+        Ok(())
     }
 }
 
 #[account]
-struct DataShape {
-    pub num: u32
+pub struct NewAccount {
+    data: u32,
 }
-#[derive(Accounts)]
 
-pub struct Initialize<'info>{
-    #[account(init, payer= signer, space = 8+4)]
-    pub account: Account<'info, DataShape>,
+#[derive(Accounts)]
+pub struct Initialize<'info> {
+    #[account(init, payer = signer, space = 8 + 4)]
+    pub new_account: Account<'info, NewAccount>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
-    #[account(mut)]
-    pub signer: Signer<'info>
 }
 
 #[derive(Accounts)]
-
-pub struct Double<'info>{
+pub struct Double<'info> {
     #[account(mut)]
-    pub account: Account<'info, DataShape>,
-    #[account(mut)]
-    pub signer: Signer<'info>
+    pub account: Account<'info, NewAccount>,
+    pub signer: Signer<'info>,
 }
 
 #[derive(Accounts)]
+pub struct Halve<'info> {
+    #[account(mut)]
+    pub account: Account<'info, NewAccount>,
+    pub signer: Signer<'info>,
+}
 
-pub struct Add<'info>{
+#[derive(Accounts)]
+pub struct Add<'info> {
     #[account(mut)]
-    pub account: Account<'info, DataShape>,
+    pub account: Account<'info, NewAccount>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct Sub<'info> {
     #[account(mut)]
-    pub signer: Signer<'info>
+    pub account: Account<'info, NewAccount>,
+    pub signer: Signer<'info>,
 }
